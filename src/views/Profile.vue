44ae1a9c789928d79661dd1c61dbe2f76286889a
chrysthlyn <chrysthlyn@gmail.com>
2021-10-27 16:35:22 +0700

    Styling Profile.vue and Data JSON

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="profile">
-    <h1>This is an profile page</h1>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="d-flex text-start">
+            <div class="flex-shrink-0">
+              <img
+                src="../assets/photo-profile.png"
+                alt="Photo Profile"
+                class="user-pic"
+              />
+            </div>
+            <div class="flex-grow-1 ms-3">
+              <h5>{{ firstName }} {{ lastName }}</h5>
+              <p>{{ bioProfile }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import "bootstrap/dist/css/bootstrap.css";
+
 export default {
   name: "Profile",
+  data: function () {
+    return {
+      firstName: "Boas",
+      lastName: "Christian",
+      bioProfile: "Hi, my hobbies include breakfast, lunch and dinner.",
+    };
+  },
   components: {},
 };
 </script>
+
+<style scoped>
+.user-pic {
+  border-radius: 200%;
+  height: 80px;
+  width: 80px;
+}
+</style>
